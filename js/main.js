590a7796aaff69e29bc3baefb719df7047a3e6e4
crohnaro <viniciosalfa@hotmail.com>
2022-07-21 01:07:13 -0300

    iniciando codigo js para popular as options consumindo as moedas da API

---
+++ js/main.js
@@ -0,0 +1,85 @@
+/*const fetchCurrencies = () => {
+    const currenciesUrl = `https://free.currconv.com/api/v7/currencies?apiKey=cc2ed161120ff7b607bd`
+    
+    fetch(currenciesUrl)
+        .then(response => response.json())
+        .then (currencies =>{
+            console.log(currencies)
+        })
+    
+        
+
+}
+
+fetchCurrencies()*/
+
+
+const myList = document.querySelector("select");
+const currenciesUrl = `https://free.currconv.com/api/v7/currencies?apiKey=cc2ed161120ff7b607bd`
+
+fetch(currenciesUrl)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error, status = ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    for (const product of data.products) {
+      const listItem = document.createElement("li");
+      
+      const nameElement = document.createElement("strong");
+      nameElement.textContent = product.Name;
+      
+      const priceElement = document.createElement("strong");
+      priceElement.textContent = `£${product.Price}`;
+
+      listItem.append(
+        nameElement,
+        ` can be found in ${product.Location}. Cost: `,
+        priceElement,
+      );
+      myList.appendChild(listItem);
+    }
+  })
+  .catch((error) => {
+    const p = document.createElement("p");
+    p.appendChild(document.createTextNode(`Error: ${error.message}`));
+    document.body.insertBefore(p, myList);
+  });
+
+
+/*let dropdown = document.getElementById('fromCurrencie');
+dropdown.length = 0;
+
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Selecione a';
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+const url = 'https://free.currconv.com/api/v7/currencies?apiKey=cc2ed161120ff7b607bd;
+
+const request = new XMLHttpRequest();
+request.open('GET', url, true);
+
+request.onload = function() {
+  if (request.status === 200) {
+    const data = JSON.parse(request.responseText);
+    let option;
+    for (let i = 0; i < data.length; i++) {
+      option = document.createElement('option');
+      option.text = data[i].name;
+      option.value = data[i].abbreviation;
+      dropdown.add(option);
+    }
+   } else {
+    // Reached the server, but it returned an error
+  }   
+}
+
+request.onerror = function() {
+  console.error('An error occurred fetching the JSON from ' + url);
+};
+
+request.send();*/
