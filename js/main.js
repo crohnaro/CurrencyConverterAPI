9fc2747858f1a558d5e3d06d80457c48de8719e3
crohnaro <viniciosalfa@hotmail.com>
2022-07-26 21:52:08 -0300

    finalizando func responsavel por resgatar os valores de cada option selecionada

--- js/main.js
+++ js/main.js
@@ -1,61 +1,93 @@
 const currenciesUrl = `https://free.currconv.com/api/v7/currencies?apiKey=cc2ed161120ff7b607bd`; // Variavel global responsavel por pegar a lista de Currencies da API
+const fromCurrencie = document.querySelector("#fromCurrencie");
+const toCurrencie = document.querySelector("#toCurrencie");
 
-const Main = {
-  init: function () {
-    this.fetchCurrencies();
-  },
 
-  fetchCurrencies: function () {
-    //Function responsavel por popular o select da page via API
-    fetch(currenciesUrl) // usando fetch para pegar os dados via JSON
-      .then((response) => response.json())
-      .then((currencies) => {
-        const currenciesStringfy = JSON.stringify(currencies);
-        const currenciesObj = JSON.parse(currenciesStringfy);
-        const currenciesResults = currenciesObj.results;
-        const currenciesKeys = Object.values(currenciesResults);
-        const fromCurrencie = document.querySelector("#fromCurrencie");
-        const toCurrencie = document.querySelector("#toCurrencie");
+const autalizarSelect = () => {
+  if (fromCurrencie && toCurrencie) {
+    let fromCurrVal = fromCurrencie.options[fromCurrencie.selectedIndex];
+    let fromCurrValURL = fromCurrVal.value;
+    let toCurrVal = toCurrencie.options[toCurrencie.selectedIndex];
+    let toCurrValURL = toCurrVal.value;
 
-        console.log(fromCurrencie.value);
-
-        for (let i = 0; i < currenciesKeys.length; i++) {
-          const opt = document.createElement("option");
-          opt.value = currenciesKeys[i].id;
-          opt.innerHTML = `${currenciesKeys[i].id} - ${currenciesKeys[i].currencyName}`;
-
-          const opt1 = document.createElement("option");
-          opt1.value = currenciesKeys[i].id;
-          opt1.innerHTML = `${currenciesKeys[i].id} - ${currenciesKeys[i].currencyName}`;
-
-          fromCurrencie.appendChild(opt);
-          toCurrencie.appendChild(opt1);
-        }
-
-        
-        const fromCurrValue = fromCurrencie.value
-        const toCurrValue = toCurrencie.value
-        const currURL = fromCurrValue + '_' + toCurrValue
-
-        document.getElementById("convert").addEventListener("click", function(){
-
-          const calcCurrencieUrl = `https://free.currconv.com/api/v7/convert?q=${currURL}&compact=ultra&apiKey=cc2ed161120ff7b607bd`;
-          fetch(calcCurrencieUrl)
-          .then((response) => response.json())
-          .then((currValue)=>{
-            console.log(currValue);
-          }) 
-        });
-
-      });
-
-      
-  },
-
-  
+    return (fromCurrValURL, toCurrValURL)
+  }
 };
 
-Main.init();
+const optCurr = document.querySelectorAll('.optCurr')
+const valueInput = document.querySelector('#result')
+
+
+document.getElementById("convert").addEventListener("click", function () {
+  let currUrl = '' // resetar o value do option para fazer nova consulta
+  if (optCurr.length){ //checar se campos estão preenchidos
+    currUrl = ''
+    optCurr.forEach((opt)=>{
+      if (currUrl == ''){
+        currUrl += opt.value  
+      }else{
+        currUrl = currUrl + "_" + opt.value
+      }   
+    })
+  }
+  
+  console.log(currUrl)
+  const calcCurrencieUrl = `https://free.currconv.com/api/v7/convert?q=${currUrl}&compact=ultra&apiKey=cc2ed161120ff7b607bd`;
+  fetch(currenciesUrl)
+    .then((response) => response.json())
+    .then((currValue) => {
+      console.log(calcCurrencieUrl)
+    });
+});
+
+const fetchCurrencies = () => {
+  //Function responsavel por popular o select da page via API
+  fetch(currenciesUrl) // usando fetch para pegar os dados via JSON
+    .then((response) => response.json())
+    .then((currencies) => {
+      const currenciesStringfy = JSON.stringify(currencies)
+      const currenciesObj = JSON.parse(currenciesStringfy)
+      const currenciesResults = currenciesObj.results
+      const currenciesKeys = Object.values(currenciesResults)
+
+      for (let i = 0; i < currenciesKeys.length; i++) {
+        const opt = document.createElement("option")
+        opt.value = currenciesKeys[i].id
+        opt.innerHTML = `${currenciesKeys[i].id} - ${currenciesKeys[i].currencyName}`
+
+        const opt1 = document.createElement("option")
+        opt1.value = currenciesKeys[i].id
+        opt1.innerHTML = `${currenciesKeys[i].id} - ${currenciesKeys[i].currencyName}`
+
+        fromCurrencie.appendChild(opt)
+        toCurrencie.appendChild(opt1)
+      }
+    })
+}
+fetchCurrencies()
+autalizarSelect()
+
+
+//const currURL = fromCurrValue + "_" + toCurrValue;
+
+/*fromCurrencie.addEventListener("change", function handleChange(event) {
+          const fromCurrURL = event.target.value;
+          console.log(fromCurrURL);
+        });
+
+
+        toCurrencie.addEventListener("change", function handleChange(event) {
+          const toCurrURL = event.target.value;
+          console.log(toCurrURL);
+        });*/
+
+/*const fromCurrVal = fromCurrencie.options[fromCurrencie.selectedIndex].value;
+        const toCurrVal = toCurrencie.options[toCurrencie.selectedIndex].value;
+
+        console.log(toCurrVal);
+        console.log(fromCurrVal);
+
+        const currUrl = fromCurrVal + "_" + toCurrVal;*/
 
 /*const myList = document.querySelector("select");
 const currenciesUrl = `https://free.currconv.com/api/v7/currencies?apiKey=cc2ed161120ff7b607bd`
